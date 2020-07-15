2a652f4335dd7d52b07f6080c37f61a56842b35b
0918nobita <nobita.0918@gmail.com>
2020-07-15 16:00:21 +0900

    :sparkles: C# から共有ライブラリを呼び出すサンプルを追加

---
+++ csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace native_interop
+{
+    class Program
+    {
+        [DllImport("../shared_library/libmylib.so")]
+        private static extern int my_func();
+
+        static void Main(string[] args)
+        {
+            int status = my_func();
+            Console.WriteLine(status);
+        }
+    }
+}
